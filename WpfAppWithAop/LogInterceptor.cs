4e427603b694c64896051f11d75e365e6b6aea04
jys923 <jys923@gmail.com>
2024-05-21 13:22:06 +0900

    add mvvm aop sample

---
+++ WpfAppWithAop/LogInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using AspectCore.DynamicProxy;
+
+namespace WpfAppWithAop
+{
+    public class LogInterceptor : AbstractInterceptor
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            Console.WriteLine($"Calling method: {context.ImplementationMethod.Name}");
+            await next(context);
+            Console.WriteLine($"Method {context.ImplementationMethod.Name} executed");
+        }
+    }
+}
