11f2d5c60482a863e7e0e6435047be3ddd3f536b
jys923 <jys923@gmail.com>
2024-07-26 14:00:26 +0900

    add delegate void Callback()

--- SonoCap.MES.Services/HsnLibraryService.cs
+++ SonoCap.MES.Services/HsnLibraryService.cs
@@ -1,20 +1,26 @@
-﻿using SonoCap.MES.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 namespace SonoCap.MES.Services
 {
-    public static class HsnLibraryService // :  :IHsnLibraryService
+    public static class HsnLibraryService
     {
         // DLL 경로 설정
-        private const string DllPath = "YourDllFileName.dll"; // DLL 파일명에 맞게 변경
+        private const string DllPath = "./lib/x64/Hsnlibrary_END.dll";
+
+        //static HsnLibraryService()
+        //{
+        //    ipInitialize();
+        //    AppDomain.CurrentDomain.ProcessExit += CleanupOnExit!;
+        //}
+
+        //private static void CleanupOnExit(object sender, EventArgs e)
+        //{
+        //    // 소멸시 호출할 함수
+        //    ipRelease();
+        //}
 
         // initialize
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?initialize@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool initialize();
 
@@ -102,69 +108,82 @@ namespace SonoCap.MES.Services
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool load_SubSetting(int subsetting_id);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?startProbeDetection@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool startProbeDetection();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?stopProbeDetection@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool stopProbeDetection();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?isProbeExist@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool isProbeExist();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?activateProbe@Hsnlibrary@@YAXXZ")]
         public static extern void activateProbe();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?disactivateProbe@Hsnlibrary@@YAXXZ")]
         public static extern void disactivateProbe();
-
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?ipInitialize@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool ipInitialize();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?ipRelease@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool ipRelease();
-
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?isIpInitialized@Hsnlibrary@@YA_NXZ")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool isIpInitialized();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr ipRenderWithCapture(IntPtr output_final_image_buf, int final_image_buf_length, IntPtr output_raw_data_buf, int raw_data_buf_length, [MarshalAs(UnmanagedType.LPStr)] out string output_metadata);
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?ipRenderWithCapture@Hsnlibrary@@YA_KPEAX_K01AEAV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z")]
+        public static extern ulong ipRenderWithCapture(IntPtr output_final_image_buf, int final_image_buf_length, IntPtr output_raw_data_buf, int raw_data_buf_length, [MarshalAs(UnmanagedType.LPStr)] out string output_metadata);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?ipResize@Hsnlibrary@@YA_NHH@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool ipResize(int width, int height);
 
         [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr ipGetLastError(out int length);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_DeviceAttached@Hsnlibrary@@YAXV?$function@$$A6AXXZ@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_DeviceAttached(Action callback);
+        //public static extern bool registerCallback_DeviceAttached(Action callback);
+        public static extern bool registerCallback_DeviceAttached(Callback callback);
+        public delegate void Callback();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_DeviceRemoved@Hsnlibrary@@YAXV?$function@$$A6AXXZ@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_DeviceRemoved(Action callback);
+        //public static extern bool registerCallback_DeviceRemoved(Action callback);
+        public static extern bool registerCallback_DeviceRemoved(Callback2 callback);
+        
+        public delegate void Callback2();
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_ENDMotorSpeed@Hsnlibrary@@YA_NV?$function@$$A6AXHH@Z@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_ENDMotorSpeed(Action<int, int> callback);
+        //public static extern bool registerCallback_ENDMotorSpeed(Action<int, int> callback);
+        public static extern bool registerCallback_ENDMotorSpeed(IntIntCallback callback);
+        public delegate void IntIntCallback(int value, int value2);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_Loading@Hsnlibrary@@YA_NV?$function@$$A6AX_N@Z@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_Loading(Action<bool> callback);
+        //public static extern bool registerCallback_Loading(Action<bool> callback);
+        public static extern bool registerCallback_Loading(BoolCallback callback);
+        public delegate void BoolCallback(bool value);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_ProbeState@Hsnlibrary@@YA_NV?$function@$$A6AXH@Z@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_ProbeState(Action<int> callback);
+        //public static extern bool registerCallback_ProbeState(Action<int> callback);
+        public static extern bool registerCallback_ProbeState(IntCallback callback);
+        public delegate void IntCallback(int value);
 
-        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl, EntryPoint = "?registerCallback_Error@Hsnlibrary@@YA_NV?$function@$$A6AXV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@H@Z@std@@@Z")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool registerCallback_Error(Action<string, int> callback);
+        //public static extern bool registerCallback_Error(Action<string, int> callback);
+        public static extern bool registerCallback_Error(StringIntCallback callback);
+        public delegate void StringIntCallback(string value, int value2);
 
         [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.I1)]
