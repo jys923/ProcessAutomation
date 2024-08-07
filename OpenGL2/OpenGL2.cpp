c5541477b3b35a209cad429c97e907b43cd7b7ad
jys923 <jys923@gmail.com>
2024-08-07 11:18:13 +0900

    start http://www.soen.kr/

--- OpenGL2/OpenGL2.cpp
+++ OpenGL2/OpenGL2.cpp
@@ -1,34 +1,19 @@
-﻿#include <gl/glew.h>
-#include <gl/glut.h>
-#include <stdlib.h>
+﻿#include <gl/glut.h>
 
-void myinit(void) {
-	// currently, does nothing
-}
-void mykeyboard(unsigned char key, int x, int y) {
-	switch (key) {
-	case 27: // ESCAPE
-		exit(0);
-		break;
-	}
-}
-void mydisplay(void) {
-	glClearColor(1.0F, 1.0F, 1.0F, 1.0F);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+void DoDisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0, 0.5);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
 }
 
 int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
-	// replace “simple” by your student number and name 
-	glutCreateWindow("hello OpenGL!");
-	glutDisplayFunc(mydisplay);
-	glutKeyboardFunc(mykeyboard);
-	glewInit();
-	myinit();
-	glutMainLoop();
-	return 0;
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL");
+    glutDisplayFunc(DoDisplay);
+    glutMainLoop();
 }
