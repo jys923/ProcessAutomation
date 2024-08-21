ba8056520fb41fd890a1846814f463fc54313d45
jys923 <jys923@gmail.com>
2024-08-21 15:48:14 +0900

    AntiAlias

--- OpenGL3WinMain/OpenGL3WinMain.cpp
+++ OpenGL3WinMain/OpenGL3WinMain.cpp
@@ -1,65 +1,82 @@
 ﻿#include <windows.h>
 #include <gl/glut.h>
 void DoDisplay();
-void DoKeyboard(unsigned char key, int x, int y);
 void DoMenu(int value);
-GLfloat Alpha = 0.5f;
-GLenum Src = GL_SRC_ALPHA;
-GLenum Dest = GL_ONE_MINUS_SRC_ALPHA;
-
-int APIENTRY WinMain2(HINSTANCE hInstance, HINSTANCE hPrevInstance
+BOOLEAN bAlias;
+BOOLEAN bHint;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
     , LPSTR lpszCmdParam, int nCmdShow)
 {
     glutInit(&__argc, __argv);
     glutCreateWindow("OpenGL");
     glutDisplayFunc(DoDisplay);
-    glutKeyboardFunc(DoKeyboard);
     glutCreateMenu(DoMenu);
-    glutAddMenuEntry("Opaque", 1);
-    glutAddMenuEntry("Traslucent", 2);
+    glutAddMenuEntry("Alias ON", 1);
+    glutAddMenuEntry("Alias OFF", 2);
+    glutAddMenuEntry("Hint ON", 3);
+    glutAddMenuEntry("Hint OFF", 4);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
     glutMainLoop();
     return 0;
-}
-void DoKeyboard(unsigned char key, int x, int y)
-{
-    switch (key) {
-    case 'q':
-        Alpha += 0.1;
-        break;
-    case 'a':
-        Alpha -= 0.1;
-        break;
-    }
-    glutPostRedisplay();
 }
 void DoMenu(int value)
 {
     switch (value) {
     case 1:
-        Src = GL_ONE;
-        Dest = GL_ZERO;
+        bAlias = TRUE;
         break;
     case 2:
-        Src = GL_SRC_ALPHA;
-        Dest = GL_ONE_MINUS_SRC_ALPHA;
+        bAlias = FALSE;
+        break;
+    case 3:
+        bHint = TRUE;
+        break;
+    case 4:
+        bHint = FALSE;
         break;
     }
     glutPostRedisplay();
 }
 void DoDisplay()
 {
-    glClearColor(1, 1, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+    // 블랜딩이 켜져 있어야 알리아싱이 제대로 된다.
     glEnable(GL_BLEND);
-    glBlendFunc(Src, Dest);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // 안티 알리아싱 on, off
+    if (bAlias) {
+        glEnable(GL_POINT_SMOOTH);
+        glEnable(GL_LINE_SMOOTH);
+        glEnable(GL_POLYGON_SMOOTH);
+    }
+    else {
+        glDisable(GL_POINT_SMOOTH);
+        glDisable(GL_LINE_SMOOTH);
+        glDisable(GL_POLYGON_SMOOTH);
+    }
+    // 고품질 출력을 위한 힌트
+    glHint(GL_POINT_SMOOTH_HINT, bHint ? GL_NICEST : GL_FASTEST);
+    glHint(GL_LINE_SMOOTH_HINT, bHint ? GL_NICEST : GL_FASTEST);
+    glHint(GL_POLYGON_SMOOTH_HINT, bHint ? GL_NICEST : GL_FASTEST);
+    glPointSize(10.0);
+    glColor3f(1, 1, 1);
+    glBegin(GL_POINTS);
+    glVertex2f(0.0, 0.8);
+    glEnd();
+    glLineWidth(5);
+    glBegin(GL_LINE_STRIP);
+    glVertex2f(-0.8, 0.7);
+    glVertex2f(0.8, 0.5);
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(1, 0, 0);
+    glVertex2f(0.0, 0.4);
+    glColor3f(0, 1, 0);
+    glVertex2f(-0.4, 0.0);
     glColor3f(0, 0, 1);
-    glRectf(-0.5, 0.8, 0.5, 0.0);
-    glColor4f(1, 0, 0, Alpha);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.0, 0.5);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(0.5, -0.5);
+    glVertex2f(0.0, -0.4);
+    glColor3f(1, 1, 0);
+    glVertex2f(0.4, 0.0);
     glEnd();
     glFlush();
 }
