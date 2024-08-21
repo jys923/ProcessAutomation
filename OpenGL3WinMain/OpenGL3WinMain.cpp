7251a765e03fb209c2568eba57175a96c48c5ca0
jys923 <jys923@gmail.com>
2024-08-21 16:51:53 +0900

    glutMenu

--- OpenGL3WinMain/OpenGL3WinMain.cpp
+++ OpenGL3WinMain/OpenGL3WinMain.cpp
@@ -1,85 +1,61 @@
 ﻿#include <windows.h>
 #include <gl/glut.h>
-#include <stdio.h>
 void DoDisplay();
-void DoKeyboard(unsigned char key, int x, int y);
-void DoSpecial(int key, int x, int y);
-void DoMouse(int button, int state, int x, int y);
-const GLfloat size = 0.2;
-const GLfloat step = 0.01;
-GLfloat nx, ny;
-GLboolean bGray = GL_FALSE;
+void DoMenu(int value);
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
     , LPSTR lpszCmdParam, int nCmdShow)
 {
     glutInit(&__argc, __argv);
     glutCreateWindow("OpenGL");
+    // 서브 메뉴 미리 준비
+    GLint SubMenu = glutCreateMenu(DoMenu);
+    glutAddMenuEntry("Red", 4);
+    glutAddMenuEntry("Green", 5);
+    glutAddMenuEntry("Blue", 6);
+    // 메인 메뉴 생성
+    glutCreateMenu(DoMenu);
+    glutAddMenuEntry("White", 1);
+    glutAddMenuEntry("Black", 2);
+    glutAddMenuEntry("Gray", 3);
+    // 서브 메뉴를 메인 메뉴에 붙인다.
+    glutAddSubMenu("Triangle Color", SubMenu);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
     glutDisplayFunc(DoDisplay);
-    glutKeyboardFunc(DoKeyboard);
-    glutSpecialFunc(DoSpecial);
-    glutMouseFunc(DoMouse);
+    glColor3f(1.0, 0.0, 0.0);
     glutMainLoop();
     return 0;
 }
-void DoKeyboard(unsigned char key, int x, int y)
+void DoMenu(int value)
 {
-    switch (key) {
-    case 'r':
-    case 'R':
-        glClearColor(1.0, 0.0, 0.0, 1.0);
+    switch (value) {
+    case 1:
+        glClearColor(1.0, 1.0, 1.0, 1.0);
         break;
-    case 'g':
-    case 'G':
-        glClearColor(0.0, 1.0, 0.0, 1.0);
+    case 2:
+        glClearColor(0.0, 0.0, 0.0, 1.0);
         break;
-    case 'b':
-    case 'B':
-        glClearColor(0.0, 0.0, 1.0, 1.0);
+    case 3:
+        glClearColor(0.5, 0.5, 0.5, 1.0);
+        break;
+    case 4:
+        glColor3f(1.0, 0.0, 0.0);
+        break;
+    case 5:
+        glColor3f(0.0, 1.0, 0.0);
+        break;
+    case 6:
+        glColor3f(0.0, 0.0, 1.0);
         break;
     }
     glutPostRedisplay();
-}
-void DoSpecial(int key, int x, int y)
-{
-    switch (key) {
-    case GLUT_KEY_LEFT:
-        nx -= step;
-        break;
-    case GLUT_KEY_RIGHT:
-        nx += step;
-        break;
-    case GLUT_KEY_UP:
-        ny += step;
-        break;
-    case GLUT_KEY_DOWN:
-        ny -= step;
-        break;
-    }
-    char info[128];
-    sprintf_s(info, "x=%.2f, y=%.2f", nx, ny);
-    glutSetWindowTitle(info);
-    glutPostRedisplay();
-}
-void DoMouse(int button, int state, int x, int y)
-{
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        bGray = !bGray;
-        glutPostRedisplay();
-    }
 }
 void DoDisplay()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    if (bGray) {
-        glColor3f(0.5, 0.5, 0.5);
-    }
-    else {
-        glColor3f(1.0, 1.0, 1.0);
-    }
-    glBegin(GL_POLYGON);
-    glVertex2f(nx, ny + size);
-    glVertex2f(nx - size, ny - size);
-    glVertex2f(nx + size, ny - size);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0, 0.5);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
     glEnd();
     glFlush();
 }
