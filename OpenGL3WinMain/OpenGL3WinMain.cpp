ef24c8a72fe6e94197f9eb6e811a6b2bd2ddba1a
jys923 <jys923@gmail.com>
2024-08-22 13:50:48 +0900

    VertexArrayIndex

--- OpenGL3WinMain/OpenGL3WinMain.cpp
+++ OpenGL3WinMain/OpenGL3WinMain.cpp
@@ -1,11 +1,9 @@
 ﻿#include <windows.h>
 #include <gl/glut.h>
 #include <stdio.h>
-
 void DoDisplay();
 void DoKeyboard(unsigned char key, int x, int y);
 GLfloat xAngle, yAngle, zAngle;
-
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 	, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -16,7 +14,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 	glutMainLoop();
 	return 0;
 }
-
 void DoKeyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
@@ -33,41 +30,36 @@ void DoKeyboard(unsigned char key, int x, int y)
 	glutSetWindowTitle(info);
 	glutPostRedisplay();
 }
-
 void DoDisplay()
 {
-	static GLfloat vert[] = {
-		0, 0, -0.8,			// 중앙
-		0.5, 0.5, 0,		// 우상
-		-0.5, 0.5, 0,		// 좌상
-		-0.5, -0.5, 0,		// 좌하
-		0.5, -0.5, 0,		// 우하
-	};
-
-	static GLubyte index[] = {
-		0, 1, 2,			//12시
-		0, 2, 3,			// 9시
-		0, 3, 4,			// 6시
-		0, 4, 1,			// 3시
-	};
-
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glRotatef(xAngle, 1.0f, 0.0f, 0.0f);
-	glRotatef(yAngle, 0.0f, 1.0f, 0.0f);
-	glRotatef(zAngle, 0.0f, 0.0f, 1.0f);
-
-	glRectf(-0.5, 0.5, 0.5, -0.5);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, vert);
-
-	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_BYTE, index);
-
-	glPopMatrix();
-	glFlush();
+    static GLfloat vertcolor[] = {
+         1,1,1,  0, 0, -0.8,          // 중앙
+         0,0,1,  0.5, 0.5, 0,    // 우상
+         1,0,0,  -0.5, 0.5, 0,   // 좌상
+         1,1,0,  -0.5, -0.5, 0,  // 좌하
+         0,1,0,  0.5, -0.5, 0,   // 우하
+    };
+    static GLubyte index[] = {
+         0, 1, 2,              //12시
+         0, 2, 3,              // 9시
+         0, 3, 4,              // 6시
+         0, 4, 1,              // 3시
+    };
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_FLAT);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glRotatef(xAngle, 1.0f, 0.0f, 0.0f);
+    glRotatef(yAngle, 0.0f, 1.0f, 0.0f);
+    glRotatef(zAngle, 0.0f, 0.0f, 1.0f);
+    glColor3f(1, 1, 1);
+    glRectf(-0.5, 0.5, 0.5, -0.5);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, sizeof(GLfloat) * 6, &vertcolor[3]);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer(3, GL_FLOAT, sizeof(GLfloat) * 6, &vertcolor[0]);
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_BYTE, index);
+    glPopMatrix();
+    glFlush();
 }
-
