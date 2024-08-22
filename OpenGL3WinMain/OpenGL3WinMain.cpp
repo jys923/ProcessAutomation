13e8ac68c3619c45b1df48ddcf89c87bbb506a5e
jys923 <jys923@gmail.com>
2024-08-22 11:41:03 +0900

    glutObject

--- OpenGL3WinMain/OpenGL3WinMain.cpp
+++ OpenGL3WinMain/OpenGL3WinMain.cpp
@@ -1,54 +1,91 @@
 ﻿#include <windows.h>
 #include <gl/glut.h>
+#include <stdio.h>
 void DoDisplay();
-void DoMenu(int value);
-GLboolean bEdge = GL_FALSE;
+void DoDisplay2();
+void DoKeyboard(unsigned char key, int x, int y);
+GLfloat xAngle, yAngle, zAngle;
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
-    , LPSTR lpszCmdParam, int nCmdShow)
+	, LPSTR lpszCmdParam, int nCmdShow)
 {
-    glutInit(&__argc, __argv);
-    glutCreateWindow("OpenGL");
-    glutDisplayFunc(DoDisplay);
-    glutCreateMenu(DoMenu);
-    glutAddMenuEntry("Edeg ON", 1);
-    glutAddMenuEntry("Edeg OFF", 2);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-    glutMainLoop();
-    return 0;
+	glutInit(&__argc, __argv);
+	glutCreateWindow("OpenGL");
+	glutDisplayFunc(DoDisplay2);
+	glutKeyboardFunc(DoKeyboard);
+	glutMainLoop();
+	return 0;
 }
-void DoMenu(int value)
+void DoKeyboard(unsigned char key, int x, int y)
 {
-    switch (value) {
-    case 1:
-        bEdge = GL_TRUE;
-        break;
-    case 2:
-        bEdge = GL_FALSE;
-        break;
-    }
-    glutPostRedisplay();
+	switch (key) {
+	case 'a':yAngle += 2; break;
+	case 'd':yAngle -= 2; break;
+	case 'w':xAngle += 2; break;
+	case 's':xAngle -= 2; break;
+	case 'q':zAngle += 2; break;
+	case 'e':zAngle -= 2; break;
+	case 'z':xAngle = yAngle = zAngle = 0.0; break;
+	}
+	char info[128];
+	sprintf_s(info, "x=%.1f, y=%.1f, z=%.1f", xAngle, yAngle, zAngle);
+	glutSetWindowTitle(info);
+	glutPostRedisplay();
 }
 void DoDisplay()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glEdgeFlag(TRUE);
-    glBegin(GL_TRIANGLES);
-    if (bEdge) glEdgeFlag(TRUE);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(-0.5, 0.5);
-    if (bEdge) glEdgeFlag(FALSE);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(0.0, 0.0);
-    if (bEdge) glEdgeFlag(TRUE);
-    glVertex2f(0.5, -0.5);
-    glVertex2f(0.5, 0.5);
-    if (bEdge) glEdgeFlag(FALSE);
-    glVertex2f(0.0, 0.0);
-    if (bEdge) glEdgeFlag(TRUE);
-    glVertex2f(-0.5, -0.5);
-    if (bEdge) glEdgeFlag(FALSE);
-    glVertex2f(0.5, -0.5);
-    glEnd();
-    glFlush();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(xAngle, 1.0f, 0.0f, 0.0f);
+	glRotatef(yAngle, 0.0f, 1.0f, 0.0f);
+	glRotatef(zAngle, 0.0f, 0.0f, 1.0f);
+	GLUquadricObj* pQuad;
+	pQuad = gluNewQuadric();
+	gluQuadricDrawStyle(pQuad, GLU_LINE);
+	gluSphere(pQuad, 0.3, 20, 20);
+	glPushMatrix();
+	glTranslatef(-0.6, 0.6, 0.0);
+	gluCylinder(pQuad, 0.2, 0.2, 0.5, 20, 20);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-0.6, -0.6, 0.0);
+	gluCylinder(pQuad, 0.2, 0.0, 0.5, 20, 20);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.6, 0.6, 0.0);
+	gluDisk(pQuad, 0.1, 0.3, 10, 10);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.6, -0.6, 0.0);
+	gluDisk(pQuad, 0.0, 0.3, 10, 10);
+	glPopMatrix();
+	gluDeleteQuadric(pQuad);
+	glPopMatrix();
+	glFlush();
+}
+void DoDisplay2()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(xAngle, 1.0f, 0.0f, 0.0f);
+	glRotatef(yAngle, 0.0f, 1.0f, 0.0f);
+	glRotatef(zAngle, 0.0f, 0.0f, 1.0f);
+	glutWireTeapot(0.3);
+	glPushMatrix();
+	glTranslatef(-0.6, 0.6, 0.0);
+	glutWireCube(0.4);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-0.6, -0.6, 0.0);
+	glutWireSphere(0.3, 20, 20);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.6, 0.6, 0.0);
+	glutWireCone(0.3, 0.6, 20, 10);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.6, -0.6, 0.0);
+	glutWireTorus(0.1, 0.2, 20, 20);
+	glPopMatrix();
+	glPopMatrix();
+	glFlush();
 }
