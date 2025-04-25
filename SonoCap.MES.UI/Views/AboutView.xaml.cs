d3a4ce24f58e15c0114275d9ee74b3549adc81ff
jys923 <jys923@gmail.com>
2025-04-25 10:50:45 +0900

    tmp

--- SonoCap.MES.UI/Views/AboutView.xaml.cs
+++ SonoCap.MES.UI/Views/AboutView.xaml.cs
@@ -1,11 +1,11 @@
-﻿using MahApps.Metro.Controls;
+﻿using System.Windows;
 
 namespace SonoCap.MES.UI.Views
 {
     /// <summary>
     /// AboutView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class AboutView : MetroWindow
+    public partial class AboutView : Window
     {
         public AboutView()
         {
