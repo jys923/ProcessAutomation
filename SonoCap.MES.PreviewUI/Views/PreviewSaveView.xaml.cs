ae69e97aeb893ef0ab1ae649b27fccc1e234c55e
jys923 <jys923@gmail.com>
2025-05-27 15:11:45 +0900

    end preview

--- SonoCap.MES.PreviewUI/Views/PreviewSaveView.xaml.cs
+++ SonoCap.MES.PreviewUI/Views/PreviewSaveView.xaml.cs
@@ -23,24 +23,5 @@ namespace SonoCap.MES.PreviewUI.Views
         {
             InitializeComponent();
         }
-        private void SrcImg_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            SrcImg.Focus(); // 여기서 직접 포커스 줌
-        }
-
-        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // 클릭된 요소가 Image가 아니면 포커스 제거
-            //if (e.OriginalSource is not Image && Keyboard.FocusedElement is UIElement focused)
-            //{
-            //    focused.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
-            //}
-
-            if (e.OriginalSource is not FrameworkElement fe || fe.Name != "SrcImg")
-            {
-                if (Keyboard.FocusedElement is UIElement focused)
-                    focused.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
-            }
-        }
     }
 }
---
+++ SonoCap.WpfCommons/Behaviors/FocusControllerBehavior.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows;
+
+namespace SonoCap.WpfCommons.Behaviors
+{
+    public class FocusControllerBehavior : Behavior<UIElement>
+    {
+        public string FocusTargetName { get; set; } = string.Empty;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseDown += OnPreviewMouseDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewMouseDown -= OnPreviewMouseDown;
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var clicked = e.OriginalSource as DependencyObject;
+            bool isClickInsideTarget = false;
+            FrameworkElement? target = null;
+
+            while (clicked != null)
+            {
+                if (clicked is FrameworkElement fe)
+                {
+                    if (fe.Name == FocusTargetName)
+                    {
+                        isClickInsideTarget = true;
+                        target = fe;
+                        break;
+                    }
+                }
+                clicked = VisualTreeHelper.GetParent(clicked);
+            }
+
+            if (isClickInsideTarget && target != null)
+            {
+                // 명시적으로 포커스
+                target.Focus();
+            }
+            else
+            {
+                // 포커스를 없애버림
+                Keyboard.ClearFocus();
+            }
+        }
+    }
+}
