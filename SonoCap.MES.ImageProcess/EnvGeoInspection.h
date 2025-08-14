3116bc73bbbb47b0c27c46b1e23ee88589c0de30
jys923 <jys923@gmail.com>
2025-08-14 13:31:13 +0900

    envgeo clean

--- SonoCap.MES.ImageProcess/EnvGeoInspection.h
+++ SonoCap.MES.ImageProcess/EnvGeoInspection.h
@@ -3,8 +3,5 @@
 
 namespace MyOpenCVWrapper {
 	void EnvGeoInspection(System::IntPtr inputBuffer, int imageWidth, int imageHeight,System::IntPtr resultBuffer, System::IntPtr textBuffer);
-	void EnvGeoInspection2(cv::Mat& roiImage, EnvGeoResult2& result);
-	void EnvGeoInspection3(cv::Mat& roiImage, EnvGeoResult2& result);
-	void EnvGeoInspection4(cv::Mat& roiImage, EnvGeoResult2& result);
 	void EnvGeoInspection(cv::Mat& roiImage, EnvGeoResult& result);
 }
